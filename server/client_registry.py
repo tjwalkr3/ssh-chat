6057d32f358d68be8bff4313b7aa0a3e2b5c6eeb
Thomas Jones <tjwalkr3@gmail.com>
2025-11-22 14:50:17 -0700

    Extract ClientRegistry message formatter

--- server/client_registry.py
+++ server/client_registry.py
@@ -7,7 +7,7 @@ class ClientRegistry:
         self.clients = []
         self.usernames = set()
         self.lock = threading.Lock()
-    
+
     def add_client(self, channel, username, input_buffer):
         with self.lock:
             if username in self.usernames:
@@ -15,24 +15,27 @@ class ClientRegistry:
             self.usernames.add(username)
             self.clients.append((channel, username, input_buffer))
             return True
-    
+
     def remove_client(self, channel, username):
         with self.lock:
             self.clients[:] = [(ch, un, ib) for ch, un, ib in self.clients if ch != channel]
             self.usernames.discard(username)
-    
+
     def broadcast(self, message, username):
-        timestamp = datetime.now().strftime("%H:%M:%S")
-        color = self._get_color(username)
-        formatted_msg = f"{timestamp}  \x1b[{color}m{username:>{USERNAME_FIELD_WIDTH}}\x1b[0m  |  {message}\r\n"
-        
+        formatted_msg = self._format_message(username, message)
+
         with self.lock:
             for client_channel, client_username, input_buffer in self.clients:
                 redraw = input_buffer.show_prompt if client_username == username else input_buffer.redraw_input_line
                 self._send_formatted_message(client_channel, input_buffer, formatted_msg, redraw)
-    
+
     def _get_color(self, username):
         return COLORS[hash(username) % len(COLORS)]
+
+    def _format_message(self, username, message):
+        timestamp = datetime.now().strftime("%H:%M:%S")
+        color = self._get_color(username)
+        return f"{timestamp}  \x1b[{color}m{username:>{USERNAME_FIELD_WIDTH}}\x1b[0m  |  {message}\r\n"
 
     def _send_formatted_message(self, client_channel, input_buffer, formatted_msg, redraw_callback):
         try:
