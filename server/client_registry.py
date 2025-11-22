99bb8f2e64793cbba2242b59eff9eb188acff8c4
Thomas Jones <tjwalkr3@gmail.com>
2025-11-22 15:03:40 -0700

    Introduce RegisteredClient dataclass for registry

--- server/client_registry.py
+++ server/client_registry.py
@@ -1,6 +1,14 @@
 import threading
+from dataclasses import dataclass
 from datetime import datetime
 from config import COLORS, USERNAME_FIELD_WIDTH
+
+
+@dataclass
+class RegisteredClient:
+    channel: object
+    username: str
+    input_buffer: object
 
 class ClientRegistry:
     def __init__(self):
@@ -13,21 +21,22 @@ class ClientRegistry:
             if username in self.usernames:
                 return False
             self.usernames.add(username)
-            self.clients.append((channel, username, input_buffer))
+            self.clients.append(RegisteredClient(channel, username, input_buffer))
             return True
 
     def remove_client(self, channel, username):
         with self.lock:
-            self.clients[:] = [(ch, un, ib) for ch, un, ib in self.clients if ch != channel]
+            self.clients[:] = [client for client in self.clients if client.channel != channel]
             self.usernames.discard(username)
 
     def broadcast(self, message, username):
         formatted_msg = self._format_message(username, message)
 
         with self.lock:
-            for client_channel, client_username, input_buffer in self.clients:
-                redraw = input_buffer.show_prompt if client_username == username else input_buffer.redraw_input_line
-                self._send_formatted_message(client_channel, input_buffer, formatted_msg, redraw)
+            for client in self.clients:
+                input_buffer = client.input_buffer
+                redraw = input_buffer.show_prompt if client.username == username else input_buffer.redraw_input_line
+                self._send_formatted_message(client, formatted_msg, redraw)
 
     def _get_color(self, username):
         return COLORS[hash(username) % len(COLORS)]
@@ -37,10 +46,10 @@ class ClientRegistry:
         color = self._get_color(username)
         return f"{timestamp}  \x1b[{color}m{username:>{USERNAME_FIELD_WIDTH}}\x1b[0m  |  {message}\r\n"
 
-    def _send_formatted_message(self, client_channel, input_buffer, formatted_msg, redraw_callback):
+    def _send_formatted_message(self, client, formatted_msg, redraw_callback):
         try:
-            input_buffer.clear_line()
-            client_channel.send(formatted_msg.encode())
+            client.input_buffer.clear_line()
+            client.channel.send(formatted_msg.encode())
             redraw_callback()
         except Exception:
             pass
